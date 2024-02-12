6b06fa3845c82317c6c207cdd74ab31c93f9752f
santiago197 <santinacional123.sr@gmail.com>
2024-02-12 16:01:45 -0500

    Get y add bears

---
+++ src/stores/bears/bear.store.ts
@@ -0,0 +1,46 @@
+import { create } from 'zustand';
+
+interface Bear {
+	id: number;
+	name: string;
+}
+interface BearState {
+	blackBears: number;
+	polarBears: number;
+	pandaBears: number;
+
+	bears: Bear[];
+
+	increaseBlackBears: (by: number) => void;
+	increasePolarBears: (by: number) => void;
+	increasePandaBears: (by: number) => void;
+
+	doNothing: () => void;
+	addBears: () => void;
+	clearBears: () => void;
+}
+
+export const useBearStore = create<BearState>()((set) => ({
+	blackBears: 10,
+	polarBears: 0,
+	pandaBears: 0,
+
+	bears: [{ id: 1, name: 'Oso #1' }],
+
+	increaseBlackBears: (by: number) =>
+		set((state) => ({ blackBears: state.blackBears + by })),
+	increasePolarBears: (by: number) =>
+		set((state) => ({ polarBears: state.polarBears + by })),
+	increasePandaBears: (by: number) =>
+		set((state) => ({ pandaBears: state.pandaBears + by })),
+
+	doNothing: () => set((state) => ({ bears: [...state.bears] })),
+	addBears: () =>
+		set((state) => ({
+			bears: [
+				...state.bears,
+				{ id: state.bears.length + 1, name: `Oso #${state.bears.length + 1}` },
+			],
+		})),
+	clearBears: () => set({ bears: [] }),
+}));
